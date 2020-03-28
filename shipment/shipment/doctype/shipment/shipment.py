208bd7652c3f254dcb3676ca9bba57d32a655711
Kanchan Chauhan <angel143.dr@gmail.com>
2020-03-28 21:28:46 +0530

    feat(Shipment): Few enhancements

--- shipment/shipment/doctype/shipment/shipment.py
+++ shipment/shipment/doctype/shipment/shipment.py
@@ -3,8 +3,24 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
+from erpnext.accounts.party import get_party_shipping_address
+from frappe.contacts.doctype.contact.contact import get_default_contact
 
 class Shipment(Document):
 	pass
+
+@frappe.whitelist()
+def get_address(ref_doctype, docname):
+	'''
+		Return address name
+	'''	
+	return get_party_shipping_address(ref_doctype, docname)
+
+@frappe.whitelist()
+def get_contact(ref_doctype, docname):
+	'''
+		Return address name
+	'''	
+	return get_default_contact(ref_doctype, docname)	
