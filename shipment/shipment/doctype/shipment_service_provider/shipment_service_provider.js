33cb8a6222561d66b780e23a61051bf3a5620a41
jbienesdev <jbienes.dev@gmail.com>
2020-04-10 17:42:15 +0800

    feat(Shipment Service Provider): add shipment service provider doctype

---
+++ shipment/shipment/doctype/shipment_service_provider/shipment_service_provider.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, Newmatik and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Shipment Service Provider', {
+	// refresh: function(frm) {
+
+	// }
+});
