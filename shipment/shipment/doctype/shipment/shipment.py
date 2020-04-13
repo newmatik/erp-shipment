637a2522272fb91a49255fdc461e398f0a0abd53
Kanchan Chauhan <angel143.dr@gmail.com>
2020-04-13 13:47:05 +0530

    feat(Shipment Integrations): Initial setup

--- shipment/shipment/doctype/shipment/shipment.py
+++ shipment/shipment/doctype/shipment/shipment.py
@@ -7,6 +7,8 @@ import frappe
 from frappe.model.document import Document
 from erpnext.accounts.party import get_party_shipping_address
 from frappe.contacts.doctype.contact.contact import get_default_contact
+from shipment.api.let_me_ship import get_letmeship_available_services
+from shipment.api.packlink import get_packlink_available_services
 
 class Shipment(Document):
 	pass
@@ -22,5 +24,15 @@ def get_address(ref_doctype, docname):
 def get_contact(ref_doctype, docname):
 	'''
 		Return address name
+	'''
+	return get_default_contact(ref_doctype, docname)
+
+@frappe.whitelist()
+def fetch_shipping_rates(pickup_from_type, delivery_to_type, pickup_address_name, delivery_address_name,
+		shipment_parcel, description_of_content, pickup_date, value_of_goods, pickup_contact_name = None, delivery_contact_name = None):
+	'''
+		Return Shipping Rates for the various Shipping Providers
 	'''	
-	return get_default_contact(ref_doctype, docname)	
+	letmeship_prices = get_letmeship_available_services()
+	packlink_prices = get_packlink_available_services()
+	return letmeship_prices, packlink_prices
