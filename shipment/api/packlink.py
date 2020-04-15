98479e1e8a6e4b2265f8fd62e9965e8ebdc6fa0f
Kanchan Chauhan <angel143.dr@gmail.com>
2020-04-15 18:30:42 +0530

    feat(LetMeShip): LetMeShip Integration

---
+++ shipment/api/packlink.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2018, ESO Electronic Service Ottenbreit GmbH
+# For license information, please see license.txt
+
+
+def get_packlink_available_services():
+	return
