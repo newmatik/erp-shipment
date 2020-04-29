4bfe2b73e8b2bf4e88ec443ffae99b9d49954da6
KanchanChauhan <angel143.dr@gmail.com>
2020-04-29 13:01:52 +0530

    refactor(Shipment): Hide button if shipment id exists
    re: #58

--- shipment/shipment/doctype/shipment/shipment.js
+++ shipment/shipment/doctype/shipment/shipment.js
@@ -120,7 +120,7 @@ frappe.ui.form.on('Shipment', {
 		})
 	},
 	refresh: function(frm) {
-		if (frm.doc.docstatus==1) {
+		if (frm.doc.docstatus==1 && !frm.doc.shipment_id) {
 			frm.add_custom_button(__('Fetch Shipping Rates'), function() {
 				return frm.events.fetch_shipping_rates(frm);
 			});
